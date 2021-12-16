903c0f388c2f7d631093107ed164511d20b5734e
devlynknelson <devlynknelson@gmail.com>
2021-12-16 15:25:28 -0800

    added a test for centered invalid catch primitive

--- bondrewd-derive/tests/enum_derives.rs
+++ bondrewd-derive/tests/enum_derives.rs
@@ -53,3 +53,27 @@ fn enum_centered_catch_all() {
     assert!(CenteredInvalid::from_primitive(154u8).into_primitive() == 2);
     assert!(CenteredInvalid::from_primitive(255u8).into_primitive() == 2);
 }
+
+#[derive(BitfieldEnum)]
+enum CenteredInvalidPrimitive {
+    Zero,
+    One,
+    Invalid(u8),
+    Three,
+    Four,
+}
+
+#[test]
+fn enum_centered_catch_primitive() {
+    assert!(CenteredInvalidPrimitive::from_primitive(0u8).into_primitive() == 0);
+    assert!(CenteredInvalidPrimitive::from_primitive(1u8).into_primitive() == 1);
+    assert!(CenteredInvalidPrimitive::from_primitive(2u8).into_primitive() == 2);
+    assert!(CenteredInvalidPrimitive::from_primitive(3u8).into_primitive() == 3);
+    assert!(CenteredInvalidPrimitive::from_primitive(4u8).into_primitive() == 4);
+
+    // test the catch all functionality
+    assert_eq!(CenteredInvalidPrimitive::from_primitive(5u8).into_primitive(), 5);
+    assert!(CenteredInvalidPrimitive::from_primitive(6u8).into_primitive() == 6);
+    assert!(CenteredInvalidPrimitive::from_primitive(154u8).into_primitive() == 154);
+    assert!(CenteredInvalidPrimitive::from_primitive(255u8).into_primitive() == 255);
+}
