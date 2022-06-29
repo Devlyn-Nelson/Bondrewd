d1f06009fa94bbaccdcfaca768e76778c163c9e9
devlynknelson <devlynknelson@gmail.com>
2022-06-28 18:27:30 -0700

    touched up the Error docs

--- bondrewd/src/error.rs
+++ bondrewd/src/error.rs
@@ -8,10 +8,13 @@ use core::fmt;
 use std::fmt;
 
 /// Error type describing that not enough bytes were provided in a slice.
-/// The first value contains the provided amount of bytes.
-/// The second value contains the expected amount of bytes.
 #[derive(Debug)]
-pub struct BitfieldSliceError(pub usize, pub usize);
+pub struct BitfieldSliceError(
+    /// Amount of provided Bytes.
+    pub usize,
+    /// Amount of expected Bytes.
+    pub usize
+);
 
 impl fmt::Display for BitfieldSliceError {
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -27,10 +30,13 @@ impl fmt::Display for BitfieldSliceError {
 impl std::error::Error for BitfieldSliceError {}
 
 /// Error type describing that a character in provided slice is Invalid.
-/// The first value contains the Invalid character.
-/// The second value contains the index of the Invalid character.
 #[derive(Debug)]
-pub struct BitfieldHexError(pub char, pub usize);
+pub struct BitfieldHexError(
+    /// The Invalid character.
+    pub char,
+    /// The index of the Invalid character.
+    pub usize
+);
 
 impl fmt::Display for BitfieldHexError {
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
