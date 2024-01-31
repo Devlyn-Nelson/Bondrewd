bbb042dce6fd24e7557e241c25cc3de5b3eac812
devlynknelson <devlynknelson@gmail.com>
2024-01-31 13:32:14 -0800

    clean up

--- bondrewd-derive/src/structs/gen.rs
+++ bondrewd-derive/src/structs/gen.rs
@@ -1,7 +1,8 @@
 //! This file is an effort to merge from and into bytes, which is being delayed for now.
+use proc_macro2::Ident;
 use quote::{format_ident, quote};
 
-use super::common::{EnumInfo, FieldInfo, StructInfo};
+use super::common::{BitMath, EnumInfo, FieldInfo, StructInfo};
 
 pub struct GeneratedFunctions {
     /// Functions that belong in `Bitfields` impl for object.
@@ -15,95 +16,95 @@ pub struct GeneratedFunctions {
     pub bitfield_dyn_trait_impl_fns: proc_macro2::TokenStream,
 }
 
-pub fn generate_functions_enum(info: &EnumInfo) -> Result<GeneratedFunctions, syn::Error> {
-    // function for getting the id of an enum.
-    let mut id_fn = quote! {};
-    let mut bitfield_trait_impl_fns = quote! {};
-    let mut impl_fns = quote! {};
-    #[cfg(feature = "dyn_fns")]
-    let mut bitfield_dyn_trait_impl_fns = quote! {};
+// pub fn generate_functions_enum(info: &EnumInfo) -> Result<GeneratedFunctions, syn::Error> {
+//     // function for getting the id of an enum.
+//     let mut id_fn = quote! {};
+//     let mut bitfield_trait_impl_fns = quote! {};
+//     let mut impl_fns = quote! {};
+//     #[cfg(feature = "dyn_fns")]
+//     let mut bitfield_dyn_trait_impl_fns = quote! {};
 
-    let from = {
-        let field = info.generate_id_field()?;
-        let flip = false;
-        let field_extractor = get_field_quote(
-            &field,
-            if flip {
-                // condition use to be `info.attrs.flip` i think this only applies to the variants
-                // and id_position is what is used here. but it should be done none the less.
-                Some(info.total_bytes() - 1)
-            } else {
-                None
-            },
-        )?;
-        let attrs = info.attrs.attrs.clone();
-        let mut fields = vec![field.clone()];
-        fields[0].attrs.bit_range = 0..info.total_bits();
-        let temp_struct_info = StructInfo {
-            name: info.name.clone(),
-            attrs,
-            fields,
-            vis: syn::Visibility::Public(Pub::default()),
-            tuple: false,
-        };
-        let id_field = generate_read_field_fn(&field_extractor, &field, &temp_struct_info, &None);
-        #[cfg(feature = "dyn_fns")]
-        {
-            let id_slice_peek =
-                generate_read_slice_field_fn(&field_extractor, &field, &temp_struct_info, &None);
-            quote! {
-                #id_field
-                #id_slice_peek
-            }
-        }
-        #[cfg(not(feature = "dyn_fns"))]
-        {
-            quote! {
-                #id_field
-            }
-        }
-    };
+//     let from = {
+//         let field = info.generate_id_field()?;
+//         let flip = false;
+//         let field_extractor = get_field_quote(
+//             &field,
+//             if flip {
+//                 // condition use to be `info.attrs.flip` i think this only applies to the variants
+//                 // and id_position is what is used here. but it should be done none the less.
+//                 Some(info.total_bytes() - 1)
+//             } else {
+//                 None
+//             },
+//         )?;
+//         let attrs = info.attrs.attrs.clone();
+//         let mut fields = vec![field.clone()];
+//         fields[0].attrs.bit_range = 0..info.total_bits();
+//         let temp_struct_info = StructInfo {
+//             name: info.name.clone(),
+//             attrs,
+//             fields,
+//             vis: syn::Visibility::Public(Pub::default()),
+//             tuple: false,
+//         };
+//         let id_field = generate_read_field_fn(&field_extractor, &field, &temp_struct_info, &None);
+//         #[cfg(feature = "dyn_fns")]
+//         {
+//             let id_slice_peek =
+//                 generate_read_slice_field_fn(&field_extractor, &field, &temp_struct_info, &None);
+//             quote! {
+//                 #id_field
+//                 #id_slice_peek
+//             }
+//         }
+//         #[cfg(not(feature = "dyn_fns"))]
+//         {
+//             quote! {
+//                 #id_field
+//             }
+//         }
+//     };
 
-    let into = {
-        let (field_setter, clear_quote) = get_field_quote(
-            &field,
-            if flip {
-                // condition use to be `info.attrs.flip` i think this only applies to the variants
-                // and id_position is what is used here. but it should be done none the less.
-                Some(info.total_bytes() - 1)
-            } else {
-                None
-            },
-            false,
-        )?;
-        let id_field = generate_write_field_fn(
-            &field_setter,
-            &field,
-            &StructInfo {
-                name: info.name.clone(),
-                attrs,
-                fields,
-                vis: syn::Visibility::Public(Pub::default()),
-                tuple: false,
-            },
-            &clear_quote,
-            &None,
-        );
-        let out = quote! {
-            #id_field
-        };
-        let out = {
-            let q = make_checked_mut_func(&info.name, info.total_bytes());
-            quote! {
-                #out
-                #q
-            }
-        };
-        out
-    };
+//     let into = {
+//         let (field_setter, clear_quote) = get_field_quote(
+//             &field,
+//             if flip {
+//                 // condition use to be `info.attrs.flip` i think this only applies to the variants
+//                 // and id_position is what is used here. but it should be done none the less.
+//                 Some(info.total_bytes() - 1)
+//             } else {
+//                 None
+//             },
+//             false,
+//         )?;
+//         let id_field = generate_write_field_fn(
+//             &field_setter,
+//             &field,
+//             &StructInfo {
+//                 name: info.name.clone(),
+//                 attrs,
+//                 fields,
+//                 vis: syn::Visibility::Public(Pub::default()),
+//                 tuple: false,
+//             },
+//             &clear_quote,
+//             &None,
+//         );
+//         let out = quote! {
+//             #id_field
+//         };
+//         let out = {
+//             let q = make_checked_mut_func(&info.name, info.total_bytes());
+//             quote! {
+//                 #out
+//                 #q
+//             }
+//         };
+//         out
+//     };
 
-    todo!("finish merged (from AND into) generate functions");
-}
+//     todo!("finish merged (from AND into) generate functions");
+// }
 struct FieldQuotes {
     get: proc_macro2::TokenStream,
     set: proc_macro2::TokenStream,
@@ -204,42 +205,28 @@ struct FieldQuotes {
 //     }
 // }
 
+struct QuoteInfo {
+    math: BitMath,
+    field_buffer_name: Ident,
+}
+
 impl FieldInfo {
-    fn get_quotes(&self, struct_info: &StructInfo) {
+    fn get_quotes(&self, struct_info: &StructInfo) -> Result<FieldQuotes, syn::Error> {
         let flip = struct_info.get_flip();
-        let (amount_of_bits, zeros_on_left, available_bits_in_first_byte, mut starting_inject_byte) =
-            BitMath::from_field(field)?.into_tuple();
+        let mut math = BitMath::from_field(self)?;
         let operator = if let Some(flip) = flip {
-            starting_inject_byte = flip - starting_inject_byte;
+            math.starting_inject_byte = flip - math.starting_inject_byte;
             quote! {-}
         } else {
             quote! {+}
         };
         // make a name for the buffer that we will store the number in byte form
-        let field_buffer_name = format_ident!("{}_bytes", field.ident().ident());
-        // check if we need to span multiple bytes
-        if amount_of_bits > available_bits_in_first_byte {
-            if self.attrs.endianness.is_none() {
-                if 8 < available_bits_in_first_byte % 8 {
-                    return Err(syn::Error::new(
-                        field.ident.span(),
-                        "calculating none-endianness `right_shift` failed",
-                    ));
-                }
-            } else {
-                if amount_of_bits < available_bits_in_first_byte {
-                    return Err(syn::Error::new(
-                        field.ident.span(),
-                        "calculating `bits_in_last_bytes` failed",
-                    ));
-                }
-            }
-            // calculate how many of the bits will be inside the least significant byte we are adding to.
-            // this will also be the number used for shifting to the right >> because that will line up
-            // our bytes for the buffer.
-            let bits_in_last_byte = (amount_of_bits - available_bits_in_first_byte) % 8;
-        } else {
-            todo!("finish single byte generated code logic.")
-        }
+        let field_buffer_name = format_ident!("{}_bytes", self.ident().ident());
+
+        // TODO from here we need to split the logic into:
+        // - get_quotes_le
+        // - get_quotes_be
+        // - get_quotes_ne
+        todo!("finish FieldInfo::get_quotes");
     }
 }
