cd3815d831496bf09253086aa258219ac4ed3e18
devlynknelson <devlynknelson@gmail.com>
2025-04-08 16:39:29 -0700

    started paring for builder

--- bondrewd-builder/src/build/field_set.rs
+++ bondrewd-builder/src/build/field_set.rs
@@ -1,8 +1,11 @@
-use syn::Ident;
+use proc_macro2::{Span, TokenStream};
+use syn::{spanned::Spanned, DeriveInput, Error, Ident};
 
 use crate::solved::field_set::SolvedFieldSetAttributes;
 
 use super::field::DataBuilder;
+
+use darling::{FromDeriveInput, FromVariant};
 
 /// Builds a bitfield model. This is not the friendliest user facing entry point for `bondrewd-builder`.
 /// please look at either [`FieldSetBuilder`] or [`EnumBuilder`] for a more user friendly builder.
@@ -12,21 +15,17 @@ pub struct GenericBuilder {
     /// Define if we are building a single `field_set` or variant type containing
     /// multiple `field_sets` switched by an id field.
     pub ty: BuilderType,
-    /// Is it a tuple struct/variant
-    pub tuple: bool,
 }
 
 impl GenericBuilder {
     pub fn from_struct_builder(s: Box<StructBuilder>) -> Self {
         Self {
             ty: BuilderType::Struct(s),
-            tuple: false,
         }
     }
     pub fn from_struct_builder_derive(s: Box<StructBuilder>, tuple: bool) -> Self {
         Self {
             ty: BuilderType::Struct(s),
-            tuple,
         }
     }
     #[must_use]
@@ -36,6 +35,89 @@ impl GenericBuilder {
     pub fn get_mut(&mut self) -> &mut BuilderType {
         &mut self.ty
     }
+    pub fn parse(input: &DeriveInput) -> syn::Result<Self> {
+        let darling = StructDarling::from_derive_input(input)?;
+        // determine byte enforcement if any.
+        let enforcement = if darling.enforce_full_bytes {
+            if darling.enforce_bytes.is_none() && darling.enforce_bits.is_none() {
+                return Err(Error::new(input.span(), "Please only use 1 byte enforcement attribute (enforce_full_bytes, enforce_bytes, enforce_bits)"))
+            }else{
+                StructEnforcement::EnforceFullBytes
+            }
+        }else if let Some(bytes) = darling.enforce_bytes {
+            if darling.enforce_bits.is_none() {
+                StructEnforcement::EnforceBitAmount(bytes * 8)
+            }else{
+                return Err(Error::new(input.span(), "Please only use 1 byte enforcement attribute (enforce_full_bytes, enforce_bytes, enforce_bits)"))
+
+            }
+        }else if let Some(bits) = darling.enforce_bits {
+            StructEnforcement::EnforceBitAmount(bits)
+        }else{
+            StructEnforcement::NoRules
+        };
+        // determine byte filling if any.
+        let fill_bits = if darling.fill {
+            if darling.fill_bytes.is_none() && darling.fill_bits.is_none() {
+                return Err(Error::new(input.span(), "Please only use 1 byte filling attribute (fill, fill_bits, fill_bytes)"))
+            }else{
+                FillBits::Auto
+            }
+        }else if let Some(bytes) = darling.fill_bytes {
+            if darling.fill_bits.is_none() {
+                FillBits::Bits(bytes * 8)
+            }else{
+                return Err(Error::new(input.span(), "Please only use 1 byte filling attribute (fill, fill_bits, fill_bytes)"))
+            }
+        }else if let Some(bits) = darling.fill_bits {
+            FillBits::Bits(bits)
+        }else{
+            FillBits::None
+        };
+        match input.data {
+            syn::Data::Struct(data_struct) => {
+                let tuple = matches!(data_struct.fields, syn::Fields::Unnamed(_));
+                let s = StructBuilder {
+                    field_set: FieldSetBuilder {
+                        name: darling.ident,
+                        fields: (),
+                        enforcement,
+                        fill_bits,
+                    },
+                    attrs: SolvedFieldSetAttributes { dump: darling.dump, vis: super::Visibility(darling.vis) },
+                    tuple,
+                };
+                // START_HERE we need to parse the fields.
+                Ok(Self {
+                    ty: BuilderType::Struct(Box::new(s)),
+                })
+            }
+            syn::Data::Enum(data_enum) => Err(Error::new(
+                Span::call_site(),
+                "Enum support is in development",
+            )),
+            syn::Data::Union(_) => Err(Error::new(Span::call_site(), "input can not be a union")),
+        }
+    }
+    pub fn generate(&self) -> syn::Result<TokenStream> {
+        Err(Error::new(Span::call_site(), "generate is not done"))
+    }
+}
+#[derive(Debug, FromDeriveInput, FromVariant)]
+#[darling(attributes(bondrewd))]
+pub struct StructDarling {
+    pub default_endianness: String,
+    pub reverse: bool,
+    // Below are being used.
+    pub ident: Ident,
+    pub vis: syn::Visibility,
+    pub dump: bool,
+    pub enforce_full_bytes: bool,
+    pub enforce_bytes: Option<usize>,
+    pub enforce_bits: Option<usize>,
+    pub fill_bits: Option<usize>,
+    pub fill_bytes: Option<usize>,
+    pub fill: bool,
 }
 /// Distinguishes between enums and structs or a single `field_set` vs multiple
 /// `field_sets` that switch based on an id field.
@@ -51,18 +133,25 @@ pub enum BuilderType {
 pub struct StructBuilder {
     pub(crate) field_set: FieldSetBuilder,
     pub(crate) attrs: SolvedFieldSetAttributes,
+    /// Is it a tuple struct/variant
+    pub tuple: bool,
 }
 impl From<FieldSetBuilder> for StructBuilder {
     fn from(value: FieldSetBuilder) -> Self {
         Self {
             field_set: value,
             attrs: SolvedFieldSetAttributes::default(),
+            tuple: false,
         }
     }
 }
 impl From<(FieldSetBuilder, SolvedFieldSetAttributes)> for StructBuilder {
     fn from((field_set, attrs): (FieldSetBuilder, SolvedFieldSetAttributes)) -> Self {
-        Self { field_set, attrs }
+        Self {
+            field_set,
+            attrs,
+            tuple: false,
+        }
     }
 }
 /// Builds an enum bitfield model.
